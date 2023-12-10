1c1658b20d9f35fa07de207868f81ec8d536b51f
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2023-12-10 10:53:33 +0530

    enums

--- sum.js
+++ sum.js
@@ -1,9 +1,14 @@
-function render(shape) {
-    console.log("renderd");
+"use strict";
+// Enums //
+var Arithmetic;
+(function (Arithmetic) {
+    Arithmetic[Arithmetic["Add"] = 0] = "Add";
+    Arithmetic[Arithmetic["Sub"] = 1] = "Sub";
+    Arithmetic[Arithmetic["Div"] = 2] = "Div";
+    Arithmetic[Arithmetic["Mul"] = 3] = "Mul";
+})(Arithmetic || (Arithmetic = {}));
+function calculate(a, b, type) {
+    console.log(type);
+    return 1;
 }
-render({
-    length: 10,
-    width: 5,
-    radius: 10,
-});
-export {};
+const ans = calculate(1, 2, Arithmetic.Mul);
--- sum.ts
+++ sum.ts
@@ -1,38 +1,23 @@
+        // Enums //
 
+enum Arithmetic {
 
-
-                         /*  types are usefull in union and ors */
-import { Circle } from "../../../node_modules/@mui/icons-material/index"
-
- 
-
-
-interface circle {
-    radius : number
-
+    Add,
+    Sub,
+    Div,
+    Mul
 }
 
-interface rectangle {
-    length : number
-    width: number
-
-}
-interface square {
-    length : number
-}
-
-type shape = circle | rectangle | square ;
-
-function render (shape: shape){
-    console.log("renderd");
+function calculate(a: number, b: number, type: Arithmetic){
+    console.log(type);
     
+    return 1;
 }
 
-render({
-    length: 10,
-    width: 5,
-    radius: 10,
-})
+const ans = calculate(1, 2, Arithmetic.Mul);
+
+
+
 
 
 
